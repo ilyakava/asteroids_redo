fcc03d804fedf8d4d8ef5ab009385e745ef4dd1a
Ilya Kavalerov <ilyakavalerov@gmail.com>
2013-09-09 16:30:15 -0400

    Consolidate application launching code

--- app.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-  var canvas = $("<canvas width='" + 500 +
-                 "' height='" + 500 + "'></canvas>");
-  $('body').append(canvas);
-
-  new Game(500, 500, 10).draw(canvas.get(0));
-
-});
